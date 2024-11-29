f6fd430b226af0dab302d5945dfc1b6127a967a3
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-11-29 08:51:37 +0100

    revert pendulum_ppo

--- pendulum_ppo.py
+++ pendulum_ppo.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 import gymnasium as gym
 import argparse
 
-from agent.ppo_calf import PPO_CALF
-
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
 from gymnasium.wrappers import TimeLimit
@@ -12,12 +10,8 @@ from mygym.my_pendulum import PendulumRenderFix
 # Import the custom callback from callback.py
 from callback.plotting_callback import PlottingCallback
 from stable_baselines3.common.utils import get_linear_fn
+from utilities.mlflow_logger import mlflow_monotoring, get_ml_logger
 
-
-from wrapper.calf_wrapper import CALFWrapper, CALFEnergyPendulumWrapper
-from controller.energybased import EnergyBasedController
-from stable_baselines3.common.vec_env import DummyVecEnv
-    
 
 # Initialize the argument parser
 parser = argparse.ArgumentParser(description="PPO Training and Evaluation for Pendulum")
@@ -34,98 +28,96 @@ gym.envs.registration.register(
     entry_point="mygym.my_pendulum:PendulumRenderFix",
 )
 
-# Use your custom environment for training
+@mlflow_monotoring
+def main(**kwargs):
+    # Use your custom environment for training
+    env = gym.make("PendulumRenderFix-v0")
+    if kwargs.get("use_mlflow"):
+        loggers = get_ml_logger()
+    env = TimeLimit(env, max_episode_steps=1000)  # Set a maximum number of steps per episode
 
+    # Total number of agent-environment interaction steps for training
+    total_timesteps = 500000
 
-def make_env():
-    def _init():
-        env = gym.make("PendulumRenderFix-v0")
-        env = TimeLimit(env, max_episode_steps=1000)  # Set a maximum number of steps per episode
-        env = CALFWrapper(env, 
-                                  fallback_policy=CALFEnergyPendulumWrapper(EnergyBasedController()))
-        return env
-    return _init
+    # Define the hyperparameters for PPO
+    ppo_hyperparams = {
+        "learning_rate": 5e-4,  # The step size used to update the policy network. Lower values can make learning more stable.
+        "n_steps": 4000,  # Number of steps to collect before performing a policy update. Larger values may lead to more stable updates.
+        "batch_size": 200,  # Number of samples used in each update. Smaller values can lead to higher variance, while larger values stabilize learning.
+        "gamma": 0.98,  # Discount factor for future rewards. Closer to 1 means the agent places more emphasis on long-term rewards.
+        "gae_lambda": 0.9,  # Generalized Advantage Estimation (GAE) parameter. Balances bias vs. variance; lower values favor bias.
+        "clip_range": 0.05,  # Clipping range for the PPO objective to prevent large policy updates. Keeps updates more conservative.
+        "learning_rate": get_linear_fn(5e-4, 1e-6, total_timesteps*2),  # Linear decay from 5e-5 to 1e-6
+    }
 
-env = DummyVecEnv([make_env()])
+    # More detailed explanation:
+    #
+    # learning_rate: Controls how quickly or slowly the model updates its parameters. A very low value, like 1e-6, results in slow learning, which can sometimes prevent instability.
+    # n_steps: Determines how many steps of experience are collected before updating the policy. A larger n_steps provides more data for each update but requires more memory and computation.
+    # batch_size: The number of samples used to compute each gradient update. It affects the variance of the gradient estimate and the stability of learning.
+    # gamma: The discount factor, which defines how future rewards are weighted relative to immediate rewards. A high value (close to 1) makes the agent focus on long-term rewards.
+    # gae_lambda: A parameter used in the Generalized Advantage Estimation (GAE) method, which helps reduce variance in the advantage estimates. It controls the trade-off between bias and variance.
+    # clip_range: The range within which the policy is clipped to prevent overly large updates, ensuring more stable training.
 
-# Total number of agent-environment interaction steps for training
-total_timesteps = 500000
+    # Check if the --notrain flag is provided
+    if not args.notrain:
 
-# Define the hyperparameters for PPO
-ppo_hyperparams = {
-    "learning_rate": 5e-4,  # The step size used to update the policy network. Lower values can make learning more stable.
-    "n_steps": 4000,  # Number of steps to collect before performing a policy update. Larger values may lead to more stable updates.
-    "batch_size": 200,  # Number of samples used in each update. Smaller values can lead to higher variance, while larger values stabilize learning.
-    "gamma": 0.98,  # Discount factor for future rewards. Closer to 1 means the agent places more emphasis on long-term rewards.
-    "gae_lambda": 0.9,  # Generalized Advantage Estimation (GAE) parameter. Balances bias vs. variance; lower values favor bias.
-    "clip_range": 0.05,  # Clipping range for the PPO objective to prevent large policy updates. Keeps updates more conservative.
-    "learning_rate": get_linear_fn(5e-4, 1e-6, total_timesteps*2),  # Linear decay from 5e-5 to 1e-6
-}
+        # Create the PPO model with the specified hyperparameters
+        model = PPO(
+            "MlpPolicy",
+            env,
+            learning_rate=ppo_hyperparams["learning_rate"],
+            n_steps=ppo_hyperparams["n_steps"],
+            batch_size=ppo_hyperparams["batch_size"],
+            gamma=ppo_hyperparams["gamma"],
+            gae_lambda=ppo_hyperparams["gae_lambda"],
+            clip_range=ppo_hyperparams["clip_range"],
+            verbose=1,
+        )
+        if kwargs.get("use_mlflow"):    
+            model.set_logger(loggers)
 
-# More detailed explanation:
-#
-# learning_rate: Controls how quickly or slowly the model updates its parameters. A very low value, like 1e-6, results in slow learning, which can sometimes prevent instability.
-# n_steps: Determines how many steps of experience are collected before updating the policy. A larger n_steps provides more data for each update but requires more memory and computation.
-# batch_size: The number of samples used to compute each gradient update. It affects the variance of the gradient estimate and the stability of learning.
-# gamma: The discount factor, which defines how future rewards are weighted relative to immediate rewards. A high value (close to 1) makes the agent focus on long-term rewards.
-# gae_lambda: A parameter used in the Generalized Advantage Estimation (GAE) method, which helps reduce variance in the advantage estimates. It controls the trade-off between bias and variance.
-# clip_range: The range within which the policy is clipped to prevent overly large updates, ensuring more stable training.
+        # Create the plotting callback
+        plotting_callback = PlottingCallback()
 
-# Check if the --notrain flag is provided
-if not args.notrain:
+        # Train the model
+        print("Training the model...")
+        model.learn(total_timesteps=total_timesteps, callback=plotting_callback)
+        # Save the model after training
+        model.save("ppo_pendulum")
+        # Close the plot after training
+        plt.ioff()  # Turn off interactive mode
+        # plt.show()  # Show the final plot
+        # plt.close("all")   
+    else:
+        print("Skipping training phase...")
 
-    # Create the PPO model with the specified hyperparameters
-    model = PPO_CALF(
-        "MlpPolicy",
-        env,
-        learning_rate=ppo_hyperparams["learning_rate"],
-        n_steps=ppo_hyperparams["n_steps"],
-        batch_size=ppo_hyperparams["batch_size"],
-        gamma=ppo_hyperparams["gamma"],
-        gae_lambda=ppo_hyperparams["gae_lambda"],
-        clip_range=ppo_hyperparams["clip_range"],
-        verbose=1,
-    )
+    # ====Evaluation: animated plot to show trained agent's performance
 
-    # Create the plotting callback
-    plotting_callback = PlottingCallback()
+    # Now enable rendering with pygame for testing
+    import pygame
+    env = gym.make("PendulumRenderFix-v0", render_mode="human")
 
-    # Train the model
-    print("Training the model...")
-    model.learn(total_timesteps=total_timesteps, callback=plotting_callback)
-    # Save the model after training
-    model.save("ppo_pendulum")
-    # Close the plot after training
-    plt.ioff()  # Turn off interactive mode
-    # plt.show()  # Show the final plot
-    # plt.close("all")   
-else:
-    print("Skipping training phase...")
+    # Load the model (if needed)
+    model = PPO.load("ppo_pendulum")
 
-# ====Evaluation: animated plot to show trained agent's performance
+    # Reset the environment
+    obs, _ = env.reset()
 
-print("Evaluation...")
-# Now enable rendering with pygame for testing
-# import pygame
-env = gym.make("PendulumRenderFix-v0", render_mode="human")
+    # Initialize pygame and set the display size
+    pygame.init()
+    # screen = pygame.display.set_mode((800, 600))  # Adjust the dimensions as needed
 
-# Load the model (if needed)
-model = PPO_CALF.load("ppo_pendulum")
+    # Run the simulation and render it
+    for _ in range(500):
+        action, _ = model.predict(obs)
+        obs, reward, done, _, _ = env.step(action)
+        env.render()  # This should now work correctly with "human" mode
+        if done:
+            obs, _ = env.reset()
 
-# Reset the environment
-obs, _ = env.reset()
+    # Close the environment after the simulation
+    env.close()
 
-# Initialize pygame and set the display size
-# pygame.init()
-# screen = pygame.display.set_mode((800, 600))  # Adjust the dimensions as needed
-
-# Run the simulation and render it
-for _ in range(500):
-    action, _ = model.predict(obs)
-    obs, reward, done, _, _ = env.step(action)
-    env.render()  # This should now work correctly with "human" mode
-    if done:
-        obs, _ = env.reset()
-
-# Close the environment after the simulation
-env.close()
+if __name__ == "__main__":
+    main()
