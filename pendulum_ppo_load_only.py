c10db78117abd5a2c9d5ba24a4b7aa5c58089aac
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-12-05 13:20:37 +0100

    refactor pendulum_ppo with calf_wrapper

--- pendulum_ppo_load_only.py
+++ pendulum_ppo_load_only.py
@@ -2,28 +2,34 @@ import matplotlib
 import matplotlib.pyplot as plt
 import gymnasium as gym
 import argparse
+import pandas as pd
+import os
 
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.callbacks import CheckpointCallback, CallbackList
 from gymnasium.wrappers import TimeLimit
 from mygym.my_pendulum import PendulumRenderFix
-# Import the custom callback from callback.py
-from callback.plotting_callback import PlottingCallback
 from stable_baselines3.common.utils import get_linear_fn
-from utilities.mlflow_logger import mlflow_monotoring, get_ml_logger
 
 from wrapper.calf_wrapper import CALFWrapper, CALFEnergyPendulumWrapper
 from controller.energybased import EnergyBasedController
-from wrapper.pendulum_wrapper import AddTruncatedFlagWrapper
 from stable_baselines3.common.vec_env import DummyVecEnv
 
-import pandas as pd
-import os
-import numpy as np
-
-
 os.makedirs("logs", exist_ok=True)
+
+
+# Initialize the argument parser
+parser = argparse.ArgumentParser(description="PPO Training and Evaluation for Pendulum")
+parser.add_argument("--console", action="store_true", help="Disable graphical output for console-only mode")
+parser.add_argument("--notrain", action="store_true", help="Skip the training phase")
+parser.add_argument("--loadstep", 
+                    type=int,
+                    help="Choose step to load checkpoint")
+parser.add_argument("--seed", 
+                    type=int,
+                    help="Choose random seed",
+                    default=42)
+# Parse the arguments
+args = parser.parse_args()
 
 matplotlib.use("TkAgg")  # Try "Qt5Agg" if "TkAgg" doesn't work
 
@@ -33,236 +39,115 @@ gym.envs.registration.register(
     entry_point="mygym.my_pendulum:PendulumRenderFix",
 )
 
-def parse_args(configs):
-        # Initialize the argument parser
-    parser = argparse.ArgumentParser(description="PPO Training and Evaluation for Pendulum")
-    parser.add_argument("--console", action="store_true", help="Disable graphical output for console-only mode")
-    parser.add_argument("--log", action="store_true", help="Enable logging and printing of simulation data.")
-    parser.add_argument("--notrain", 
-                        action="store_true", 
-                        help="Skip the training phase",
-                        default=True)
+# Use your custom environment for training
+env = gym.make("PendulumRenderFix-v0")
 
-    parser.add_argument("--loadstep", 
-                        type=int,
-                        help="Choose step to load checkpoint",
-                        default=configs["total_timesteps"])
+env = TimeLimit(env, max_episode_steps=1000)  # Set a maximum number of steps per episode
 
-    parser.add_argument("--seed", 
-                        type=int,
-                        help="Choose random seed",
-                        default=42)
-    
-    # CALF hyperparameter
-    # relax_prob_base_step_factor
-    # relax_prob_episode_factor
-    return parser.parse_args()
+# Total number of agent-environment interaction steps for training
+total_timesteps = 500000
 
-    parser.add_argument("--calf_decay_rate", 
-                        type=int,
-                        help="Choose desired decay rate of state value which supports the main condition to activate CALF",
-                        default=0.005)
-    
-    parser.add_argument("--initial_relax_prob", 
-                        type=int,
-                        help="Choose initial relax probability",
-                        default=0.7)
-    
-    parser.add_argument("--relax_prob_base_step_factor",
-                        type=int,
-                        help="Choose initial relax probability",
-                        default=0.7)
+# Define the hyperparameters for PPO
+ppo_hyperparams = {
+    "learning_rate": 5e-4,  # The step size used to update the policy network. Lower values can make learning more stable.
+    "n_steps": 4000,  # Number of steps to collect before performing a policy update. Larger values may lead to more stable updates.
+    "batch_size": 200,  # Number of samples used in each update. Smaller values can lead to higher variance, while larger values stabilize learning.
+    "gamma": 0.98,  # Discount factor for future rewards. Closer to 1 means the agent places more emphasis on long-term rewards.
+    "gae_lambda": 0.9,  # Generalized Advantage Estimation (GAE) parameter. Balances bias vs. variance; lower values favor bias.
+    "clip_range": 0.05,  # Clipping range for the PPO objective to prevent large policy updates. Keeps updates more conservative.
+    "learning_rate": get_linear_fn(5e-4, 1e-6, total_timesteps*2),  # Linear decay from 5e-5 to 1e-6
+    "use_sde": True, # Whether to use generalized State Dependent Exploration (gSDE) instead of action noise exploration (default: False)
+    "sde_sample_freq": 4, # Sample a new noise matrix every n steps when using gSDE
+}
 
-    # Parse the arguments
-    args = parser.parse_args()
-    return args
+# More detailed explanation:
+#
+# learning_rate: Controls how quickly or slowly the model updates its parameters. A very low value, like 1e-6, results in slow learning, which can sometimes prevent instability.
+# n_steps: Determines how many steps of experience are collected before updating the policy. A larger n_steps provides more data for each update but requires more memory and computation.
+# batch_size: The number of samples used to compute each gradient update. It affects the variance of the gradient estimate and the stability of learning.
+# gamma: The discount factor, which defines how future rewards are weighted relative to immediate rewards. A high value (close to 1) makes the agent focus on long-term rewards.
+# gae_lambda: A parameter used in the Generalized Advantage Estimation (GAE) method, which helps reduce variance in the advantage estimates. It controls the trade-off between bias and variance.
+# clip_range: The range within which the policy is clipped to prevent overly large updates, ensuring more stable training.
 
-@mlflow_monotoring()
-def main(**kwargs):
-    # Use your custom environment for training
-    env = gym.make("PendulumRenderFix-v0")
-    if kwargs.get("use_mlflow"):
-        loggers = get_ml_logger()
-    env = TimeLimit(env, max_episode_steps=1000)  # Set a maximum number of steps per episode
+print("Skipping training phase...")
 
-    # Total number of agent-environment interaction steps for training
-    total_timesteps = 500000
-
-    args = parse_args(configs={
-        "total_timesteps": total_timesteps
-    })
-
-    # Define the hyperparameters for PPO
-    ppo_hyperparams = {
-        "learning_rate": 5e-4,  # The step size used to update the policy network. Lower values can make learning more stable.
-        "n_steps": 4000,  # Number of steps to collect before performing a policy update. Larger values may lead to more stable updates.
-        "batch_size": 200,  # Number of samples used in each update. Smaller values can lead to higher variance, while larger values stabilize learning.
-        "gamma": 0.98,  # Discount factor for future rewards. Closer to 1 means the agent places more emphasis on long-term rewards.
-        "gae_lambda": 0.9,  # Generalized Advantage Estimation (GAE) parameter. Balances bias vs. variance; lower values favor bias.
-        "clip_range": 0.05,  # Clipping range for the PPO objective to prevent large policy updates. Keeps updates more conservative.
-        "learning_rate": get_linear_fn(5e-4, 1e-6, total_timesteps*2),  # Linear decay from 5e-5 to 1e-6
-    }
-
-    calf_hyperparams = {
-        "calf_decay_rate": 0.005,
-        "initial_relax_prob": 0.7,
-        "relax_prob_base_step_factor": 0.99,
-        "relax_prob_episode_factor": 0.0
-    }
-
-    # More detailed explanation:
-    #
-    # learning_rate: Controls how quickly or slowly the model updates its parameters. A very low value, like 1e-6, results in slow learning, which can sometimes prevent instability.
-    # n_steps: Determines how many steps of experience are collected before updating the policy. A larger n_steps provides more data for each update but requires more memory and computation.
-    # batch_size: The number of samples used to compute each gradient update. It affects the variance of the gradient estimate and the stability of learning.
-    # gamma: The discount factor, which defines how future rewards are weighted relative to immediate rewards. A high value (close to 1) makes the agent focus on long-term rewards.
-    # gae_lambda: A parameter used in the Generalized Advantage Estimation (GAE) method, which helps reduce variance in the advantage estimates. It controls the trade-off between bias and variance.
-    # clip_range: The range within which the policy is clipped to prevent overly large updates, ensuring more stable training.
-
-    
-
-    # Check if the --notrain flag is provided
-    if not args.notrain:
-
-        # Create the PPO model with the specified hyperparameters
-        model = PPO(
-            "MlpPolicy",
+# ====Evaluation: animated plot to show trained agent's performance
+def make_env(seed):
+    def _init():
+        env = PendulumRenderFix(render_mode="human" if not args.console else None)
+        # env = PendulumRenderFix()
+        # env = TimeLimit(env, max_episode_steps=1000)  # Set a maximum number of steps per episode
+        env = CALFWrapper(
             env,
-            learning_rate=ppo_hyperparams["learning_rate"],
-            n_steps=ppo_hyperparams["n_steps"],
-            batch_size=ppo_hyperparams["batch_size"],
-            gamma=ppo_hyperparams["gamma"],
-            gae_lambda=ppo_hyperparams["gae_lambda"],
-            clip_range=ppo_hyperparams["clip_range"],
-            verbose=1,
+            fallback_policy=CALFEnergyPendulumWrapper(EnergyBasedController()),
+            calf_decay_rate=0.01,
+            initial_relax_prob=0.5,
+            relax_prob_base_step_factor=0.95,
+            relax_prob_episode_factor=0.,
+            debug=False,
         )
-        if kwargs.get("use_mlflow"):    
-            model.set_logger(loggers)
+        
+        env.reset(seed=seed)
+        
+        return env
+    return _init
+# Now enable rendering with pygame for testing
+import pygame
+env_agent = DummyVecEnv([make_env(args.seed)])
 
-        # Create the plotting callback
-        plotting_callback = PlottingCallback()
-
-        checkpoint_callback = CheckpointCallback(
-            save_freq=1000,  # Save the model periodically
-            save_path="./checkpoints",  # Directory to save the model
-            name_prefix="ppo_pendulum"
-            )
-
-        # Combine both callbacks using CallbackList
-        callback = CallbackList([
-            checkpoint_callback,
-            plotting_callback,
-            # gradient_monitor_callback
-            ])
-
-        # Train the model
-        print("Training the model...")
-        model.learn(total_timesteps=total_timesteps, callback=callback)
-        # Save the model after training
-        model.save("ppo_pendulum")
-        # Close the plot after training
-        plt.ioff()  # Turn off interactive mode
-        # plt.show()  # Show the final plot
-        # plt.close("all")   
-    else:
-        print("Skipping training phase...")
-
-    # ====Evaluation: animated plot to show trained agent's performance
-    
-    def make_env(seed, options):
-        def _init():
-            env = PendulumRenderFix(render_mode="human" if not args.console else None)
-            # env = PendulumRenderFix()
-            # env = TimeLimit(env, max_episode_steps=1000)  # Set a maximum number of steps per episode
-            env = CALFWrapper(
-                env,
-                fallback_policy=CALFEnergyPendulumWrapper(EnergyBasedController()),
-                calf_decay_rate=0.01,
-                initial_relax_prob=0.5,
-                relax_prob_base_step_factor=0.95,
-                relax_prob_episode_factor=0.,
-                debug=False,
-                logger=loggers
-            )
-            
-            env.reset(seed=seed, options=options)
-            
-            return env
-        return _init
-    
-    # Now enable rendering with pygame for testing
-    import pygame
-    
-    np.random.seed(args.seed)
-    high, low = env.observation_space.high, env.observation_space.low
-    options = {
-        "angle": np.random.uniform(-np.pi, np.pi),
-        "angular_velocity": np.random.uniform(low[-1], high[-1]),
-    }
-
-    env_agent = DummyVecEnv([make_env(args.seed, options)])
-
-    # Load the model (if needed)
+# Load the model (if needed)
+if not args.loadstep:
+    model = PPO.load("ppo_pendulum")
+else:
     model = PPO.load(f"checkpoints/ppo_pendulum_{args.loadstep}_steps")
-    if loggers:
-        model.set_logger(loggers)
 
+# Reset the environment
+env_agent.seed(seed=args.seed)
+obs = env_agent.reset()
+env_agent.env_method("copy_policy_model", model.policy)
 
-    # Reset the environments
-    env_agent.set_options(options=options)
-    env_agent.seed(seed=args.seed)
-    obs = env_agent.reset()
-    env_agent.env_method("copy_policy_model", model.policy)
+# Initialize pygame and set the display size
+pygame.init()
+# screen = pygame.display.set_mode((800, 600))  # Adjust the dimensions as needed
+info_dict = {
+    "state": [],
+    "action": [],
+    "reward": [],
+    "accumulated_reward": [],
+    "relax_probability": [],
+    "calf_activated_count": [],
+}
+accumulated_reward = 0
 
-    # Run the simulation with the trained agent
-    # for _ in range(3000):
-    
-    info_dict = {
-        "state": [],
-        "action": [],
-        "reward": [],
-        "accumulated_reward": [],
-        "relax_probability": [],
-        "calf_activated_count": [],
-    }
-    accumulated_reward = 0
-    n_step = 1000
+# Run the simulation and render it
+for _ in range(1000):
+    action, _ = model.predict(obs)
+    # Dynamically handle four or five return values
+    result = env_agent.step(action)  # Take a step in the environment
+    if len(result) == 4:
+        obs, reward, done, info = result
+        truncated = False
+    else:
+        obs, reward, done, truncated, info = result
 
-    for step_i in range(n_step):
-        action, _ = model.predict(obs)
+    accumulated_reward += reward
 
-        # Dynamically handle four or five return values
-        result = env_agent.step(action)  # Take a step in the environment
-        if len(result) == 4:
-            obs, reward, done, info = result
-            truncated = False
-        else:
-            obs, reward, done, truncated, info = result
+    info_dict["state"].append(obs)
+    info_dict["action"].append(action)
+    info_dict["reward"].append(reward)
+    info_dict["accumulated_reward"].append(accumulated_reward.copy())
+    info_dict["relax_probability"].append(env_agent.get_attr("relax_prob").copy()[0])
+    info_dict["calf_activated_count"].append(env_agent.get_attr("calf_activated_count").copy()[0])
 
-        accumulated_reward += reward
+# Close the environment after the simulation
+env_agent.close()
 
-        info_dict["state"].append(obs[0])
-        info_dict["action"].append(action[0])
-        info_dict["reward"].append(reward)
-        info_dict["accumulated_reward"].append(accumulated_reward.copy())
-        info_dict["relax_probability"].append(env_agent.get_attr("relax_prob").copy()[0])
-        info_dict["calf_activated_count"].append(env_agent.get_attr("calf_activated_count").copy()[0])
-        model.logger.dump(step_i)
-        if done:
-            model.logger.dump(n_step)
-            obs = env_agent.reset()  # Reset the agent's environment
+df = pd.DataFrame(info_dict)
+checkpoint_name = args.loadstep if args.loadstep else "last_checkpoint"
+file_name = f"pure_ppo_with_calfw_eval_{checkpoint_name}_seed_{args.seed}.csv"
 
-    # Close the environments
-    env_agent.close()
+if args.log:
+    df.to_csv("logs/" + file_name)
 
-    df = pd.DataFrame(info_dict)
-    file_name = f"pure_ppo_with_calfw_eval_{args.loadstep}_seed_{args.seed}.csv"
-
-    if args.log:
-        df.to_csv("logs/" + file_name)
-
-    print("Case:", file_name)
-    print(df.tail(2))
-
-if __name__ == "__main__":
-    main()
+print("Case:", file_name)
+print(df.tail(2))
