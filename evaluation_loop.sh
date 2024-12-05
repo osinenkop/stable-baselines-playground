ee73b677c90d62efdd63c3b1c1b22e94ddd5e0cf
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-12-05 11:49:35 +0100

    update config and analysis notebook for the latest eval

--- evaluation_loop.sh
+++ evaluation_loop.sh
@@ -1,16 +1,16 @@
-for i in $(seq 10 40)
+for i in $(seq 11 40)
 do
     # Eval last checkpoint of pure PPO
     python pendulum_ppo.py --notrain --seed $i --console --log
 
     # Eval checkpoint at step 200k of pure PPO
-    python pendulum_ppo.py --notrain --loadstep 250000 --seed $i --console --log
+    python pendulum_ppo.py --notrain --loadstep 200000 --seed $i --console --log
 
     # Eval last checkpoint of pure PPO + CALF wrapper
     python pendulum_ppo_load_only.py --notrain  --seed $i --console --log
 
     # Eval checkpoint at step 200k of pure PPO + CALF wrapper
-    python pendulum_ppo_load_only.py --notrain --loadstep 250000 --seed $i --console --log
+    python pendulum_ppo_load_only.py --notrain --loadstep 200000 --seed $i --console --log
 
     # Get data of EnergyBasedController as a reference
     python pendulum_controller.py --seed $i --console --log
