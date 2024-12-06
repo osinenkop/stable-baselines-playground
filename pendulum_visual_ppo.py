6e551919e18714f42906832af558f8b95ea4c735
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-12-06 11:11:55 +0100

    add console option to eval

--- pendulum_visual_ppo.py
+++ pendulum_visual_ppo.py
@@ -84,6 +84,9 @@ def main(**kwargs):
     parser.add_argument("--console", action="store_true", help="Disable graphical output for console-only mode")
     parser.add_argument("--normalize", action="store_true", help="Enable observation and reward normalization")
     parser.add_argument("--single-thread", action="store_true", help="Use DummyVecEnv for single-threaded environment")
+    parser.add_argument("--loadstep", 
+                        type=int,
+                        help="Choose step to load checkpoint")
     parser.add_argument("--seed", 
                         type=int,
                         help="Choose random seed",
