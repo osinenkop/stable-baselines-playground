3e6e4e7f9bc3935a40bd73cf9d890137c862ffa8
Huy Hoang <huyhoang251296@gmail.com>
2024-12-11 14:23:36 +0100

    update eval and notebooks

--- evaluation_visual_loop.sh
+++ evaluation_visual_loop.sh
@@ -1,25 +1,31 @@
 for i in $(seq 11 40)
 do
         PYTHONFAULTHANDLER=1 python pendulum_visual_ppo_load_only.py \
-                --fallback-checkpoint "backups/2024-12-09 101010/ppo_visual_pendulum.zip" \
-                --eval-checkpoint "backups/2024-12-09 101010/ppo_visual_pendulum.zip" \
-                --eval-name "fallback_100_agent_100" \
+                --fallback-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_655360_steps.zip" \
+                --eval-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_655360_steps.zip" \
+                --eval-name "fallback_50_agent_50" \
                 --log --console --seed $i
 
         PYTHONFAULTHANDLER=1 python pendulum_visual_ppo_load_only.py \
-                --fallback-checkpoint "backups/2024-12-09 101010/ppo_visual_pendulum.zip" \
+                --fallback-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_327680_steps.zip" \
                 --eval-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_327680_steps.zip" \
-                --eval-name "fallback_100_agent_25" \
+                --eval-name "fallback_25_agent_25" \
                 --log --console --seed $i
 
-        PYTHONFAULTHANDLER=1 python pendulum_visual_ppo.py --notrain \
-                --eval-checkpoint "backups/2024-12-09 101010/ppo_visual_pendulum.zip" \
-                --eval-name "well_trained" \
+        PYTHONFAULTHANDLER=1 python pendulum_visual_ppo_load_only.py \
+                --fallback-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_327680_steps.zip" \
+                --eval-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_655360_steps.zip" \
+                --eval-name "fallback_25_agent_50" \
                 --log --console --seed $i
 
-        PYTHONFAULTHANDLER=1 python pendulum_visual_ppo.py --notrain \
-                --eval-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_327680_steps.zip" \
-                --eval-name "under_trained_25" \
-                --log --console --seed $i
+        # PYTHONFAULTHANDLER=1 python pendulum_visual_ppo.py --notrain \
+        #         --eval-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_655360_steps.zip" \
+        #         --eval-name "agent_50" \
+        #         --log --console --seed $i
+
+        # PYTHONFAULTHANDLER=1 python pendulum_visual_ppo.py --notrain \
+        #         --eval-checkpoint "backups/2024-12-09 101010/checkpoints/ppo_visual_pendulum_327680_steps.zip" \
+        #         --eval-name "agent_25" \
+        #         --log --console --seed $i
 
 done
