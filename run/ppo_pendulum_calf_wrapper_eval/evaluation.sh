75515485f77aac4db06707f10da5a91a2b81347e
antonbolychev <bolychev.anthon@yandex.ru>
2024-12-12 19:17:33 +0300

    fix(run/ppo_pendulum_calf_wrapper_eval/evaluation): rename python files' names in bash

--- run/ppo_pendulum_calf_wrapper_eval/evaluation.sh
+++ run/ppo_pendulum_calf_wrapper_eval/evaluation.sh
@@ -5,10 +5,10 @@ python pendulum_ppo.py --notrain --loadstep 500000 --console
 python pendulum_ppo.py --notrain --loadstep 200000 --console
 
 # Eval last checkpoint of pure PPO + CALF wrapper
-python pendulum_ppo_load_only.py --notrain --loadstep 500000 --console
+python pendulum_ppo_eval_calf_wrapper.py --notrain --loadstep 500000 --console
 
 # Eval checkpoint at step 200k of pure PPO + CALF wrapper
-python pendulum_ppo_load_only.py --notrain --loadstep 200000 --console
+python pendulum_ppo_eval_calf_wrapper.py --notrain --loadstep 200000 --console
 
 # Get data of EnergyBasedController as a reference
 python pendulum_controller.py --console
--- run/ppo_pendulum_calf_wrapper_eval/evaluation_loop.sh
+++ run/ppo_pendulum_calf_wrapper_eval/evaluation_loop.sh
@@ -7,10 +7,10 @@ do
     python pendulum_ppo.py --notrain --loadstep 200000 --seed $i --console --log
 
     # Eval last checkpoint of pure PPO + CALF wrapper
-    python pendulum_ppo_load_only.py --notrain --loadstep 500000 --seed $i --console --log
+    python pendulum_ppo_eval_calf_wrapper.py --notrain --loadstep 500000 --seed $i --console --log
 
     # Eval checkpoint at step 200k of pure PPO + CALF wrapper
-    python pendulum_ppo_load_only.py --notrain --loadstep 200000 --seed $i --console --log
+    python pendulum_ppo_eval_calf_wrapper.py --notrain --loadstep 200000 --seed $i --console --log
 
     # Get data of EnergyBasedController as a reference
     python pendulum_controller.py --seed $i --console --log
