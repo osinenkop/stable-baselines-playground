06eb6a95ee832388ac1cc919e0b2c798d8ea18d8
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-12-13 18:42:22 +0100

    refactor(run/ppo_vispendulum_self_boost): rename checkpoints ppo_vispendulum* in bash scripts

--- run/ppo_vispendulum_self_boost/evaluation_visual.sh
+++ run/ppo_vispendulum_self_boost/evaluation_visual.sh
@@ -1,15 +1,15 @@
 PYTHONFAULTHANDLER=1 python ppo_vispendulum_eval_calf_wrapper.py \
-    --fallback-checkpoint "./artifacts/checkpoints/ppo_visual_pendulum_327680_steps.zip" \
-    --eval-checkpoint "./artifacts/checkpoints/ppo_visual_pendulum_655360_steps.zip" \
+    --fallback-checkpoint "./artifacts/checkpoints/ppo_vispendulum_327680_steps.zip" \
+    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_655360_steps.zip" \
     --eval-name "fallback_25_agent_50" \
     --log --console --seed 22
 
 PYTHONFAULTHANDLER=1 python ppo_vispendulum.py --notrain \
-    --eval-checkpoint "./artifacts/checkpoints/ppo_visual_pendulum_655360_steps.zip" \
+    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_655360_steps.zip" \
     --eval-name "agent_50" \
     --log --console --seed 22
 
 PYTHONFAULTHANDLER=1 python ppo_vispendulum.py --notrain \
-    --eval-checkpoint "./artifacts/checkpoints/ppo_visual_pendulum_327680_steps.zip" \
+    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_327680_steps.zip" \
     --eval-name "agent_25" \
     --log --console --seed 22
