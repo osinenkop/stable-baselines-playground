b10a3443fec5dd32094daeb86a9a52a7ebbc3cc2
osinenkop <p.osinenko@gmail.com>
2024-12-15 22:52:36 +0100

    Checked with relax prob 100%

--- run/ppo_vispendulum_self_boost/evaluation_visual_loop.sh
+++ run/ppo_vispendulum_self_boost/evaluation_visual_loop.sh
@@ -1,24 +1,24 @@
-declare -a relax_prob_arr=(0.4 0.2 0.1)
+declare -a relax_prob_arr=(1.0 0.75 0.5)
 
-for i in $(seq 105 156)
+for i in $(seq 1 5)
 do
     for relax_prob in ${relax_prob_arr[@]}
     do
     PYTHONFAULTHANDLER=1 python ppo_vispendulum_eval_calf_wrapper.py \
         --calf-init-relax $relax_prob \
-        --fallback-checkpoint "./artifacts/checkpoints/ppo_vispendulum_98304_steps.zip" \
-        --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_49152_steps.zip" \
+        --fallback-checkpoint "./artifacts/checkpoints/ppo_vispendulum_65536_steps.zip" \
+        --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_32768_steps.zip" \
         --eval-name "fallback_25_agent_50_${relax_prob}" \
         --log --console --seed $i
     done
 
 PYTHONFAULTHANDLER=1 python ppo_vispendulum.py --notrain \
-    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_98304_steps.zip" \
+    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_65536_steps.zip" \
     --eval-name "agent_50" \
     --log --console --seed $i
 
 PYTHONFAULTHANDLER=1 python ppo_vispendulum.py --notrain \
-    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_49152_steps.zip" \
+    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_32768_steps.zip" \
     --eval-name "agent_25" \
     --log --console --seed $i
 done
