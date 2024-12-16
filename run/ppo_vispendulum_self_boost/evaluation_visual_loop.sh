1ce6dbb42978b4eb45989c5b2b94a0fd7fbcc652
Huy Hoang <huyhoang251296@gmail.com>
2024-12-16 17:22:31 +0100

    feats(run/ppo_vispendulum_self_boost): update eval scripts after reverting training PPO back

--- run/ppo_vispendulum_self_boost/evaluation_visual_loop.sh
+++ run/ppo_vispendulum_self_boost/evaluation_visual_loop.sh
@@ -6,19 +6,19 @@ do
     do
     PYTHONFAULTHANDLER=1 python ppo_vispendulum_eval_calf_wrapper.py \
         --calf-init-relax $relax_prob \
-        --fallback-checkpoint "./artifacts/checkpoints/ppo_vispendulum_327680_steps.zip" \
-        --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_655360_steps.zip" \
+        --calf-fallback-checkpoint "./artifacts/checkpoints/ppo_vispendulum_32768_steps.zip" \
+        --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_65536_steps.zip" \
         --eval-name "fallback_25_agent_50_${relax_prob}" \
         --log --console --seed $i
     done
 
 PYTHONFAULTHANDLER=1 python ppo_vispendulum.py --notrain \
-    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_655360_steps.zip" \
+    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_65536_steps.zip" \
     --eval-name "agent_50" \
     --log --console --seed $i
 
 PYTHONFAULTHANDLER=1 python ppo_vispendulum.py --notrain \
-    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_327680_steps.zip" \
+    --eval-checkpoint "./artifacts/checkpoints/ppo_vispendulum_32768_steps.zip" \
     --eval-name "agent_25" \
     --log --console --seed $i
 done
